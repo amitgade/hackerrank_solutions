a7aa13ee521ce8c29f40005e12288eb3e9876581
amitgade <amitgaade@gmail.com>
2020-07-11 21:37:17 +0530

    action-Add solution If Else

---
+++ java/src/introduction/IfElse.java
@@ -0,0 +1,26 @@
+package introduction;
+
+import java.util.Scanner;
+
+/**
+ * Created by amit on 11-Jul-2020
+ */
+public class IfElse {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        String ans="";
+        if(n%2==1){
+            ans = "Weird";
+        }
+        else{
+            if(n>=6 && n<=20){
+                ans = "Weird";
+            }else{
+                ans = "Not Weird";
+            }
+        }
+        System.out.println(ans);
+        sc.close();
+    }
+}
