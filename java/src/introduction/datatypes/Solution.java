5a2b0442b395e2e99defe44cabb6d84c7325bd6b
amitgade <amitgaade@gmail.com>
2020-07-11 20:44:15 +0530

    action-Add solution to Datatypes

---
+++ java/src/introduction/datatypes/Solution.java
@@ -0,0 +1,50 @@
+package introduction.datatypes;
+
+import java.util.Scanner;
+
+/**
+ * Created by amit on 11-Jul-2020
+ */
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+
+            try {
+                long x = sc.nextLong();
+                System.out.println(x + " can be fitted in:");
+
+                try {
+                    byte y = (byte) x;
+                    if (y == x) {
+                        System.out.println("* byte");
+                    }
+                } catch (Exception e) {
+                }
+
+                try {
+                    short y = (short) x;
+                    if (y == x) {
+                        System.out.println("* short");
+                    }
+                } catch (Exception e) {
+                }
+
+                try {
+                    int y = (int) x;
+                    if (y == x) {
+                        System.out.println("* int");
+                    }
+                } catch (Exception e) {
+                }
+
+                System.out.println("* long");
+
+            } catch (Exception e) {
+                System.out.println(sc.next() + " can't be fitted anywhere.");
+            }
+        }
+    }
+}
