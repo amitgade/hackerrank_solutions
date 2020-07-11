241514d169a04fb40cec1d75781c66e8ff148b60
amitgade <amitgaade@gmail.com>
2020-07-11 21:30:02 +0530

    action-Add solution StdInAndStdOut2

---
+++ java/src/introduction/StdInAndStdOut2.java
@@ -0,0 +1,20 @@
+package introduction;
+
+import java.util.Scanner;
+
+/**
+ * Created by amit on 11-Jul-2020
+ */
+public class StdInAndStdOut2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+        scan.close();
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
