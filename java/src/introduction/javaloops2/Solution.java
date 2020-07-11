365907011ef91189f92367db71c534d720c6c232
amitgade <amitgaade@gmail.com>
2020-07-11 20:36:50 +0530

    action-Add solution to Java Loops II

---
+++ java/src/introduction/javaloops2/Solution.java
@@ -0,0 +1,25 @@
+package introduction.javaloops2;
+
+import java.util.Scanner;
+
+/**
+ * Created by amit on 11-Jul-2020
+ */
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for (int i = 0; i < t; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int calc = a;
+            for (int j = 0; j < n; j++) {
+                calc += Math.pow(2, j) * b;
+                System.out.print(calc + " ");
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
