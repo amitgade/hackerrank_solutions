df551cc65df2b5259e7e10faf54798a431c7e062
amitgade <amitgaade@gmail.com>
2020-07-11 21:22:50 +0530

    action-Add solution WelcomeToJava

---
+++ java/src/introduction/WelcomeToJava.java
@@ -0,0 +1,11 @@
+package introduction;
+
+/**
+ * Created by amit on 11-Jul-2020
+ */
+public class WelcomeToJava {
+    public static void main(String[] args) {
+        System.out.println("Hello World.");
+        System.out.println("Hello Java.");
+    }
+}
