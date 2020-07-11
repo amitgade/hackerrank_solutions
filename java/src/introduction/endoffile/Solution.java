447486c486fa5e753a83fd96ca10e3673a3828c1
amitgade <amitgaade@gmail.com>
2020-07-11 20:40:50 +0530

    action-Add solution to End Of File

---
+++ java/src/introduction/endoffile/Solution.java
@@ -0,0 +1,17 @@
+package introduction.endoffile;
+
+import java.util.Scanner;
+
+/**
+ * Created by amit on 11-Jul-2020
+ */
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = 1;
+        while (sc.hasNext()) {
+            System.out.println(i++ + " " + sc.nextLine());
+        }
+        sc.close();
+    }
+}
