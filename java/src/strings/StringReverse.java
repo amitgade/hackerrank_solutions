371bdb7dd2f7cff5940a3d3fd30e082296d45cff
amitgade <amitgaade@gmail.com>
2020-07-12 14:02:32 +0530

    action-close scanner referance

--- java/src/strings/StringReverse.java
+++ java/src/strings/StringReverse.java
@@ -5,24 +5,18 @@ import java.util.Scanner;
 /**
  * Created by amit on 12-Jul-2020
  */
-public class StringsIntroduction {
+public class StringReverse {
     public static void main(String[] args) {
+
+        /* If a String is equivalent to itself when reversed, it's a palindrome */
         Scanner sc=new Scanner(System.in);
         String A=sc.next();
-        String B=sc.next();
-        sc.close();
         /* Enter your code here. Print output to STDOUT. */
-        System.out.println(A.length() + B.length());
 
-        if (A.compareTo(B) > 0) {
+        if(A.equals(new StringBuilder(A).reverse().toString())){
             System.out.println("Yes");
         }else{
             System.out.println("No");
         }
-        System.out.println(capFirstLetter(A) + " " + capFirstLetter(B));
-    }
-
-    private static String capFirstLetter(String str) {
-        return str.substring(0,1).toUpperCase()  + str.substring(1);
     }
 }
