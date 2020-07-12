0b7377ebed541723e81bf3a66f530ccfef0ab654
amitgade <amitgaade@gmail.com>
2020-07-12 17:17:12 +0530

    action-Add solution PatternSyntaxChecker

--- java/src/strings/PatternSyntaxChecker.java
+++ java/src/strings/PatternSyntaxChecker.java
@@ -1,6 +1,8 @@
 package strings;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 /**
  * Created by amit on 12-Jul-2020
@@ -9,13 +11,16 @@ public class PatternSyntaxChecker {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int testCases = Integer.parseInt(in.nextLine());
-        while (testCases > 0) {
+        while (testCases-- > 0) {
             String pattern = in.nextLine();
             //Write your code
-
-
+            try {
+                Pattern.compile(pattern);
+                System.out.println("Valid");
+            } catch (PatternSyntaxException e) {
+                System.out.println("Invalid");
+            }
         }
-
         in.close();
     }
 }
