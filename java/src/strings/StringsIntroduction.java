0c414b267dea76d8702353b93332be4784f0c404
amitgade <amitgaade@gmail.com>
2020-07-12 13:54:50 +0530

    action-Add Solution String Introduction

---
+++ java/src/strings/StringsIntroduction.java
@@ -0,0 +1,27 @@
+package strings;
+
+import java.util.Scanner;
+
+/**
+ * Created by amit on 12-Jul-2020
+ */
+public class StringsIntroduction {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        System.out.println(A.length() + B.length());
+
+        if (A.compareTo(B) > 0) {
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+        System.out.println(capFirstLetter(A) + " " + capFirstLetter(B));
+    }
+
+    private static String capFirstLetter(String str) {
+        return str.substring(0,1).toUpperCase()  + str.substring(1);
+    }
+}
