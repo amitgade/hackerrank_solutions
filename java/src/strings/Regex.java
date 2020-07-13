c3d25453cd64005f2c65c6f254eec830f8bb8960
amitgade <amitgaade@gmail.com>
2020-07-13 11:54:09 +0530

    action-Add Solution Regex

---
+++ java/src/strings/Regex.java
@@ -0,0 +1,28 @@
+package strings;
+
+import java.util.Scanner;
+
+/**
+ * Created by amit on 13-Jul-2020
+ */
+public class Regex {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+        in.close();
+    }
+}
+
+/*
+     [01]?\\d{1,2}   matches numbers 0-199.
+     2[0-4]\\d       matches numbers 200-249
+     25[0-5]         matches numbers 250-255
+*/
+class MyRegex {
+    String num = "([01]?\\d{1,2}|2[0-4]\\d|25[0-5])";
+    String pattern = num + "." + num + "." + num + "." + num;
+}
+
