ca013d792fa522e13dc8ed946dec40d9e45edd78
amitgade <amitgaade@gmail.com>
2020-08-10 19:02:08 +0530

    action-Modify Indentation

---
+++ java/src/datastructures/Java1DArrayPart2.java
@@ -0,0 +1,10 @@
+package datastructures;
+
+/**
+ * Created by amit on 10-Aug-2020
+ */
+public class Java1DArrayPart2 {
+    public static void main(String[] args) {
+
+    }
+}
--- java/src/datastructures/JavaArrayList.java
+++ java/src/datastructures/JavaArrayList.java
@@ -30,9 +30,9 @@ public class JavaArrayList {
         for (int i = 0; i < q; i++) {
             int x = scan.nextInt();
             int y = scan.nextInt();
-            ArrayList<Integer> list = lists.get(x-1);
+            ArrayList<Integer> list = lists.get(x - 1);
             if (y <= list.size()) {
-                System.out.println(list.get(y-1));
+                System.out.println(list.get(y - 1));
             } else {
                 System.out.println("ERROR!");
             }
