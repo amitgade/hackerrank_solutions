9808ad29a03f4e8e0a1ec50159ef16737b343dfb
amitgade <amitgaade@gmail.com>
2020-08-18 14:20:11 +0530

    action-Add solution to Java Method verriding problem

---
+++ java/src/oo_programming/JavaMethodOverriding.java
@@ -0,0 +1,41 @@
+package oo_programming;
+
+/**
+ * Created by amit on 18-Aug-2020
+ */
+public class JavaMethodOverriding {
+    public static void main(String[] args) {
+        Sports c1 = new Sports();
+        Soccer c2 = new Soccer();
+        System.out.println(c1.getName());
+        c1.getNumberOfTeamMembers();
+        System.out.println(c2.getName());
+        c2.getNumberOfTeamMembers();
+    }
+
+
+}
+
+class Sports {
+
+    String getName() {
+        return "Generic Sports";
+    }
+
+    void getNumberOfTeamMembers() {
+        System.out.println("Each team has n players in " + getName());
+    }
+}
+
+
+class Soccer extends Sports {
+    @Override
+    String getName() {
+        return "Soccer Class";
+    }
+
+    // Write your overridden getNumberOfTeamMembers method here
+    void getNumberOfTeamMembers() {
+        System.out.println("Each team has 11 players in " + getName());
+    }
+}
