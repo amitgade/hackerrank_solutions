9fa066f28c29e1effdbec3cd4cd48de1ae1490bf
amitgade <amitgaade@gmail.com>
2020-08-20 14:34:47 +0530

    action-Add solution Java ExceptionHandling

---
+++ java/src/exception_handling/JavaExceptionHandling.java
@@ -0,0 +1,41 @@
+package exception_handling;
+
+import java.util.Scanner;
+
+/**
+ * Created by amit on 20-Aug-2020
+ */
+public class JavaExceptionHandling {
+    public static final MyCalculator my_calculator = new MyCalculator();
+    public static final Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        while (in.hasNextInt()) {
+            int n = in.nextInt();
+            int p = in.nextInt();
+
+            try {
+                System.out.println(my_calculator.power(n, p));
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+        in.close();
+    }
+}
+
+class MyCalculator {
+    /*
+     * Create the method long power(int, int) here.
+     */
+    public long power(int n, int p) throws Exception {
+        if (n < 0 || p < 0) {
+            throw new Exception("n or p should not be negative.");
+        } else if (n == 0 && p == 0) {
+            throw new Exception("n and p should not be zero.");
+        } else {
+            return (long) Math.pow(n, p);
+        }
+    }
+
+}
