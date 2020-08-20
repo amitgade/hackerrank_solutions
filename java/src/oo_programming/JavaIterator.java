e74c6f46a3a08a6054e24336681beeb1eb6c0daf
amitgade <amitgaade@gmail.com>
2020-08-20 14:24:27 +0530

    action-Add solution to Java Iterator

---
+++ java/src/oo_programming/JavaIterator.java
@@ -0,0 +1,42 @@
+package oo_programming;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+/**
+ * Created by amit on 20-Aug-2020
+ */
+public class JavaIterator {
+    static Iterator func(ArrayList mylist){
+        Iterator it=mylist.iterator();
+        while(it.hasNext()){
+            Object element = it.next();
+            if(element instanceof String)
+                break;
+        }
+        return it;
+
+    }
+
+    public static void main(String[] args) {
+        ArrayList mylist = new ArrayList();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        for(int i = 0;i<n;i++){
+            mylist.add(sc.nextInt());
+        }
+
+        mylist.add("###");
+        for(int i=0;i<m;i++){
+            mylist.add(sc.next());
+        }
+
+        Iterator it=func(mylist);
+        while(it.hasNext()){
+            Object element = it.next();
+            System.out.println((String)element);
+        }
+    }
+}
