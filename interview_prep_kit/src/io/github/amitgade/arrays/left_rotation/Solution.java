8756a792bd9b09504fc2779f1de39127b126a6ef
amitgade <amitgaade@gmail.com>
2022-02-24 17:41:23 +0530

    action-Add solution to left rotaion problem

---
+++ interview_prep_kit/src/io/github/amitgade/arrays/left_rotation/Solution.java
@@ -0,0 +1,75 @@
+package io.github.amitgade.arrays.left_rotation;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by amitgade on 24-Feb-2022
+ */
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int d = Integer.parseInt(firstMultipleInput[1]);
+
+        String[] aTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> a = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aTemp[i]);
+            a.add(aItem);
+        }
+
+        List<Integer> result = Result.rotLeft(a, d);
+
+        for (int i = 0; i < result.size(); i++) {
+            bufferedWriter.write(String.valueOf(result.get(i)));
+
+            if (i != result.size() - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
+class Result {
+
+    /*
+     * Complete the 'rotLeft' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER d
+     */
+    // TODO: 24/02/22  Solution starts
+    public static List<Integer> rotLeft(List<Integer> a, int d) {
+        List<Integer> rotatedList = new ArrayList<>();
+        int startIdx = d;
+        int iterationIdx = 0;
+
+        while (iterationIdx < a.size()) {
+            if (startIdx == a.size()) {
+                startIdx = 0;
+            }
+
+            rotatedList.add(iterationIdx, a.get(startIdx));
+            iterationIdx++;
+            startIdx++;
+        }
+        return rotatedList;
+    }
+    // TODO: 24/02/22 Solution ends
+}
